3eaa611f5bb01026b55381f7754356fc4bef4ead
shinbeomsik <124110586+shinbeomsik@users.noreply.github.com>
2023-02-02 17:50:33 +0900

    day04 2023/02/02

---
+++ day04/out.js
@@ -0,0 +1,23 @@
+//함수는 여러절차가 있는 처리를 이름을 부여하여 기능을 정의!
+      //function
+     //함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의하는거!
+        function 빨래하다() {
+            console.log("1.먼지를 털다.")
+            console.log("2.물에 집어 넣는다.")
+            console.log("3.비누를 바른다.")
+            console.log("4.오물조물 비빈다.")
+            console.log("5.물에 여러번 헹군다.")
+            console.log("6.짠다.")
+            console.log("7.널다.")
+        }
+      
+     function 청소하다(){
+        console.log("1.책상위를 정리한다.")
+        console.log("2.책을 정리한다.")
+        console.log("3.수납장을 정리한다.")
+        console.log("4.쓰레기통을 버린다.")
+        console.log("5.책상 위를 닦는다")
+        console.log("6.다 끝났다고 생각한다")
+        console.log("7.침대위에 눕는다")
+    }
+     
---
+++ day04/out2.js
@@ -0,0 +1,14 @@
+function 여행하다(){
+    console.log("1.여행계획하기")
+    console.log("2.여행준비하기")
+    console.log("3.여행경비정하기")
+    console.log("4.재미있게놀기")
+    console.log("5.사진 많이찍기")
+}
+
+function 쉬다() {
+    console.log("1.눕는다")
+    console.log("2.뒹굴뒹굴한다")
+    console.log("3.유투브를본다")
+    console.log("4.잔다")
+}
