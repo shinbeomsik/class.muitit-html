a957df4a156085f14fda0900ff899f03e08833b3
shinbeomsik <124110586+shinbeomsik@users.noreply.github.com>
2023-02-07 19:20:44 +0900

    day07 2023/02/07

---
+++ java02/src/복습/문제풀이5.java
@@ -0,0 +1,21 @@
+package 복습;
+
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+public class 문제풀이5 {
+
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		
+		int year = date.getYear() + 1900;
+		int day = date.getDay();
+		int month = date.getMonth() + 1;
+		
+		
+
+	}
+
+}
